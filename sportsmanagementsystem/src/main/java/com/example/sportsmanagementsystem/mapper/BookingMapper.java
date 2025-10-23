b5312c7a8755dd6c07087940bb9add6aa52de805
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-23 09:57:16 +0300

    BookingMapper

---
+++ sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/mapper/BookingMapper.java
@@ -0,0 +1,25 @@
+package com.example.sportsmanagementsystem.mapper;
+
+import com.example.sportsmanagementsystem.Dto.BookingRequest;
+import com.example.sportsmanagementsystem.Dto.BookingResponse;
+import com.example.sportsmanagementsystem.model.Booking;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BookingMapper {
+
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "sportField", ignore = true)
+    Booking toEntity(BookingRequest bookingRequest);
+
+    @Mapping(source = "sportField.name", target= "sportFieldName")
+    @Mapping(source = "status", target = "status", qualifiedByName = "mapStatusToString")
+    BookingResponse toDto(Booking booking);
+
+    List<BookingResponse> toDtoList(List<Booking> bookings);
+
+}
