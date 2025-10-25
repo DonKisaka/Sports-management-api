8792d9797dc4c3539e59288b74b5b8c8259dc889
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-25 17:33:42 +0300

    Repository

--- sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/repository/BookingRepository.java
+++ sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/repository/BookingRepository.java
@@ -4,7 +4,10 @@ import com.example.sportsmanagementsystem.model.Booking;
 import com.example.sportsmanagementsystem.model.BookingStatus;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +18,13 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findBySportFieldId(Long sportFieldId);
     Optional<Booking> findByIdAndUserEmail(Long id, String email);
 
-    boolean hasOverlappingBooking(@NotNull(message = "Sport field id must not be null") Long aLong, LocalTime start, LocalTime end);
+
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END " +
+           "FROM Booking b " +
+            "WHERE b.sportField.id = :sportFieldId AND " +
+            "((b.startDateTime < :endDateTime AND b.endDateTime > :startDateTime))")
+    boolean hasOverlappingBooking(
+            @Param("sportFieldId") Long sportFieldId,
+            @Param("startDateTime")LocalDateTime startDateTime,
+            @Param("endDateTime")LocalDateTime endDateTime);
 }
