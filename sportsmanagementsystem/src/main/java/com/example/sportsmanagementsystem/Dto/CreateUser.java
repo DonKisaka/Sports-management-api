c83a39645d45887b0123c2bb9ecbe696543cd176
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-26 07:57:18 +0300

    Bean validation

--- sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/Dto/CreateUser.java
+++ sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/Dto/CreateUser.java
@@ -1,7 +1,14 @@
 package com.example.sportsmanagementsystem.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateUser(
+        @NotBlank(message = "Email must not be blank")
+        @Email(message = "Email must be valid")
    String email,
+   @NotBlank(message = "Username must not be blank")
    String username,
+   @NotBlank(message = "Password must not be blank")
    String password
 ) {}
--- sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/Dto/LoginUser.java
+++ sportsmanagementsystem/src/main/java/com/example/sportsmanagementsystem/Dto/LoginUser.java
@@ -1,6 +1,13 @@
 package com.example.sportsmanagementsystem.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginUser(
+        @NotBlank(message = "Email must not be blank")
+        @Email(message = "Email must be valid")
    String email,
+
+   @NotBlank(message = "Password must not be blank")
    String password
 ) {}
